deb94b0a4273606ce80bd908be8757a6f03fe648
Hubol Persson-Gordon <hubol@articulate.com>
2024-10-04 16:28:38 -0500

    Cleanup

--- src/mapped-joi.ts
+++ src/mapped-joi.ts
@@ -1,20 +1,20 @@
 import Joi from "joi";
 
 export type MappedJoi<T> =
-  T extends Joi.ObjectSchema<infer TObject>
-    ? TObject
-    : T extends Joi.StringSchema
-      ? string
-      : T extends Joi.NumberSchema
-        ? number
-        : T extends Joi.BooleanSchema
-          ? boolean
-          : T extends Joi.DateSchema
-            ? Date
-            : never;
+  T extends Joi.OptionalChained<infer TOptional>
+    ? MappedJoi<TOptional> | undefined
+    : T extends Joi.ObjectSchema<infer TObject>
+      ? TObject
+      : T extends Joi.StringSchema
+        ? string
+        : T extends Joi.NumberSchema
+          ? number
+          : T extends Joi.BooleanSchema
+            ? boolean
+            : T extends Joi.DateSchema
+              ? Date
+              : never;
 
 export type MappedJoiSchema<T> = {
-  [k in keyof T]: T[k] extends Joi.OptionalChained<infer TOptional>
-    ? MappedJoi<TOptional> | undefined
-    : MappedJoi<T[k]>;
+  [k in keyof T]: MappedJoi<T[k]>;
 };
